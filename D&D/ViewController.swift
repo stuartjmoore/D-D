f58cc8a63fc9606b0a97dacda0fb36e7279a9a14
Stuart Moore <stuartjmoore@gmail.com>
2015-02-13 09:09:51 -0500

    Initial Commit

---
+++ D&D/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  D&D
+//
+//  Created by Moore, Stuart on 2/13/15.
+//  Copyright (c) 2015 Stuart Moore. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
